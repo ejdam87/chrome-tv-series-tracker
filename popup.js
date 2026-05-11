cbed31f71181e8111149475ef3640dd4179e0aba
ejdam87 <adam.dzadon@gmail.com>
2026-05-11 19:39:41 +0200

    feat: add buttons

--- popup.js
+++ popup.js
@@ -13,9 +13,37 @@ function render() {
             a.href = data[series].urlPatterns[0];
             a.target = "_blank";
             const spacer = document.createTextNode(" ");
+
+            const nextBtn = document.createElement("button");
+            nextBtn.innerText = "Increment Next Episode";
+
+            const lastBtn = document.createElement("button");
+            lastBtn.innerText = "The Last Episode of Series";
+
+            nextBtn.onclick = () => {
+                chrome.runtime.sendMessage({
+                    type: "INCREMENT_EPISODE",
+                    series
+                });
+                setTimeout(render, 100);
+            };
+
+            lastBtn.onclick = () => {
+                chrome.runtime.sendMessage({
+                    type: "LAST_EPISODE",
+                    series
+                });
+                setTimeout(render, 100);
+            };
+
+            list.append(document.createElement("hr"))
             div.appendChild(spacer);
             div.appendChild(a);
             list.appendChild(div);
+            list.append(nextBtn);
+            list.append(lastBtn);
+            list.append(document.createElement("hr"))
+
         });
     });
 }
